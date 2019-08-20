6ce199ad1caef253d28ccb15e43dbc9ef5d4de82
ham1255 <mohammedtgm.mohammedtgm@gmail.com>
2019-08-20 04:04:51 +0400

    first commit

---
+++ src/main.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class main {
+    public static void main(String[] args) {
+        System.out.println("[mbot]What's you name? in mc ofc");
+
+        Scanner Iscaner = new Scanner(System.in);
+        String McName = Iscaner.nextLine();
+
+        System.out.println("[mbot]What is fav minecraft item?");
+        String Mcitem = Iscaner.nextLine();
+        System.out.println("[mbot] Your name is " + McName);
+        System.out.println("[mbot] your fav item is " + Mcitem);
+        System.out.println("[mbot] Great. YOUR PC HAS BEEN HACKED bruh jk lol");
+
+
+
+
+
+
+
+    }
+}
