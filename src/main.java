3869be6481ac82154bb85c04a3589db83d997465
ham1255 <mohammedtgm.mohammedtgm@gmail.com>
2019-08-20 08:27:30 +0400

    changed anything to false and true only

--- src/main.java
+++ src/main.java
@@ -2,7 +2,9 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class main {
+
     public static void main(String[] args) {
+        long x;
         System.out.println("[mbot]What's you name? in mc ofc");
         Scanner Iscaner = new Scanner(System.in);
         String McName = Iscaner.nextLine();
@@ -11,11 +13,18 @@ public class main {
         System.out.println("[mbot] Your name is " + McName);
         System.out.println("[mbot] your fav item is " + Mcitem);
         System.out.println("[mbot] Great. YOUR PC HAS BEEN HACKED bruh jk lol");
-        System.out.println("[Mbot] should i spam your pc? say true or if you dont want to say false or anything you want Warning this might lag the pc");
+        System.out.println("[Mbot] should i spam your pc? say true or if you dont want to say false // Warning this might lag the pc");
         String tnt = Iscaner.nextLine();
         System.out.println(tnt);
-        long x;
         Random rand = new Random();
+        while(!tnt.equals("true")) {
+            if (tnt.equals("false")){
+                System.exit(0);
+            }
+            System.out.println("[mbot] Nope thats not a boolean! try again. ps: use false or true");
+            tnt = Iscaner.nextLine();
+
+        }
         while(tnt.equals("true")){
             x = rand.nextInt(394584544);
             System.out.println(x);
