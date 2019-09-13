2d2d8c9f24454a72b4ba1c85a184f76d32c70c2f
ham1255 <mohammedtgm.mohammedtgm@gmail.com>
2019-09-13 21:10:34 +0400

    fixed typos

--- src/com/Atlantheamc/mbot/Something.java
+++ src/com/Atlantheamc/mbot/Something.java
@@ -13,6 +13,7 @@ public class Something {
         int Year;
         int Day;
         String[] MonthsName = {"nothing here lol","January","February","March","April","May","June","July","August","September","October","November","December"};
+        String[] MonthMaxDays = {"nothing here lol","31","28","31","30","31","30","31","31","30","31","30","31"};
         LocalDate now = LocalDate.now();
         //-----------------------------\\
         System.out.println("[Mbot] Welcome to simple clac of how many days since your bday " + now );
@@ -20,8 +21,8 @@ public class Something {
         System.out.println("First type your month in numbers like 9 or 12");
         Month = Iscaner.nextInt();
         //-----------------------------\\
-        while (Month < 0 || Month > 12){
-            System.out.println("Wrong usage Month number can't be less than 0 or higher than 12");
+        while (Month <= 0 || Month > 12){
+            System.out.println("Wrong usage Month number can't be less than 1 or higher than 12");
             Month = Iscaner.nextInt();
         }
         //-----------------------------\\
@@ -40,10 +41,10 @@ public class Something {
                 System.out.println("You where born on " + Total + " so You are " + ChronoUnit.YEARS.between(Total, now) + " years old or " + ChronoUnit.MONTHS.between(Total, now) + "month old OR "  + ChronoUnit.DAYS.between(Total, now));
                 x = x + 1;
             } catch (Exception e) {
-                System.out.println("Type right day just think Your month is " + MonthsName[Month]);
+                System.out.println("Type right day just think Your month is " + MonthsName[Month] + " Which is max " + MonthMaxDays[Month] + " Days. " );
                 Day = Iscaner.nextInt();
                 Total = LocalDate.of(Year, Month, Day);
-                System.out.println("You where born on " + Total + " so You are " + ChronoUnit.YEARS.between(Total, now) + " years old or " + ChronoUnit.MONTHS.between(Total, now) + "month old OR "  + ChronoUnit.DAYS.between(Total, now));
+                System.out.println("You where born on " + Total + " so You are " + ChronoUnit.YEARS.between(Total, now) + " years old or " + ChronoUnit.MONTHS.between(Total, now) + " month old OR "  + ChronoUnit.DAYS.between(Total, now) + " Days old.");
                 x = x + 1;
             }
 
